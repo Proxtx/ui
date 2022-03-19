0d8dc714d3595eecb78b47936a0cf5f3553cd684
Proxtx <gopro54122@gmail.com>
2022-03-19 18:27:14 +0100

    building the concept

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-export const load = async () => {};
+/**
+ * Loads a module template and returns its class
+ * @param {Object} options Additional options. Must provide the location of module
+ * @return {Module} The module as class
+ */
+export const load = async (options) => {};
