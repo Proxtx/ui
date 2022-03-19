678fef7f1b8cfb2f828e5a09cf9bcc0b7e0bc18c
Justus Fluegel <klelars@outlook.de>
2022-03-19 21:50:38 +0100

    USE TS!

--- main.js
+++
@@ -1,6 +0,0 @@
-/**
- * Loads a module template and returns its class
- * @param {Object} options Additional options. Must provide the location of module
- * @return {Module} The module as class
- */
-export const load = async (options) => {};
