aa115c8a2dc98ed518848ef627882a65523eea8f
Proxtx <gopro54122@gmail.com>
2022-03-23 16:45:03 +0100

    fixed it

--- componentHandler.js
+++ componentHandler.js
@@ -10,43 +10,26 @@ console.log("yes i do exist");
  * @return {Module} The module as class
  */
 export const load = async (options) => {
-  let wrap = document.createElement("div");
   let iframe = document.createElement("iframe");
   iframe.src = options.url;
   iframe.style.border = "none";
 
-  //iframe.style.display = "none";
-  wrap.appendChild(iframe);
-  document.documentElement.appendChild(wrap);
-  await new Promise((r) => (iframe.onload = r));
+  iframe.onload = () => {
+    iframe.contentWindow.componentHandler = componentHandler;
+    iframe.contentWindow.isComponent = true;
+    iframe.contentWindow.args = options.args;
+    iframe.contentWindow.setDimension = (width, height) => {
+      iframe.width = width;
+      iframe.height = height;
+    };
 
-  iframe.contentWindow.componentHandler = componentHandler;
-  iframe.contentWindow.isComponent = true;
-  iframe.contentWindow.args = options.args;
-  iframe.contentWindow.setDimension = (width, height) => {
-    iframe.width = width;
-    iframe.height = height;
+    let script = iframe.contentDocument.createElement("script");
+    script.type = "module";
+    script.text = componentHandler;
+    iframe.contentDocument.documentElement.appendChild(script);
   };
 
-  let r;
-
-  iframe.contentWindow.loaded = () => {
-    r();
-  };
-
-  let script = iframe.contentDocument.createElement("script");
-  script.type = "module";
-  script.text = componentHandler;
-  iframe.contentDocument.documentElement.appendChild(script);
-
-  await new Promise((resolve) => (r = resolve));
-
-  await new Promise((r) => setTimeout(r, 2000));
-
-  //document.documentElement.removeChild(iframe);
-  iframe.style.display = "unset";
-
-  return { iframe: wrap };
+  return { iframe };
 };
 
 export const startup = async () => {
@@ -60,8 +43,6 @@ export const startup = async () => {
   let importObject = await import(handler.src);
 
   importObject.changeName("test 5.0");
-
-  window.loaded();
 };
 
 if (window.isComponent) startup();
